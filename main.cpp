426f0512e1ac0b292d5465f2eb8a921c1b0cab19
MikhailChekal <mishka.chekal@gmail.com>
2022-02-10 20:37:26 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <chrono>
+#include <cmath>
+
+using namespace std::chrono;
+
+template <class T>
+class Timer {
+
+private:
+    steady_clock::time_point start = steady_clock::now();
+    int dur = 0;
+public:
+    Timer() {};
+    ~Timer() {
+        pause();
+        //dur += int(duration_cast<T>(steady_clock::now() - start).count());
+        //std::cout << dur << std::endl;
+    }
+    void pause(){ // pause и cont всегда должны идти вместе
+        dur += int(duration_cast<T>(steady_clock::now() - start).count());
+        std::cout << dur << std::endl;
+    };
+    void cont(){
+        start = steady_clock::now();
+
+    };
+};
+
+int main() {
+    double result = 0.;
+    {
+        Timer<std::chrono::milliseconds> t;
+        for(auto i = 0u; i < 10000000u; i++) {
+            result += std::sin(i) + std::cos(i);
+        }
+        t.pause();
+        for(auto i = 0u; i < 10000000u; i++) {
+            result += std::sin(i) + std::cos(i);
+        }
+        t.cont();
+    }
+    std::cout << "Result is " << result;
+    return 0;
+}
